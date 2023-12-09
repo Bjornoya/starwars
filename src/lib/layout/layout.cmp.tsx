e58f9a138f15cbe6ae33045e9e48a37ed7109c2e
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-09 16:41:51 +0100

    updated the width value in the layout component

--- src/lib/layout/layout.cmp.tsx
+++ src/lib/layout/layout.cmp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from '@mui/material';
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return <Container maxWidth="md">{children}</Container>;
+  return <Container maxWidth="lg">{children}</Container>;
 }
 
 export default Layout;
