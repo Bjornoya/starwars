3b0a2dbcae6c52a4cb3d652239514796a9733e88
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-09 22:53:57 +0100

    created the 'tableSkeleton' component

---
+++ src/pages/characters/components/charactersTable/tableSkeletonRow.cmp.tsx
@@ -0,0 +1,34 @@
+import { Skeleton, TableCell, TableRow } from '@mui/material';
+
+function TableRowsLoader({ rowsNum }) {
+  return [...Array(rowsNum)].map((row, index) => (
+    <TableRow key={index}>
+      <TableCell component="th" scope="row">
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+      <TableCell>
+        <Skeleton animation="wave" variant="text" />
+      </TableCell>
+    </TableRow>
+  ));
+}
+
+export default TableRowsLoader;
