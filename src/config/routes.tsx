d535f6b9cbcce7f860313e4d5f28a60cb20a2845
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-09 15:25:40 +0100

    configured routes with redirects and error handling

---
+++ src/config/routes.tsx
@@ -0,0 +1,21 @@
+import { lazy } from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
+import NotFoundPage from '../pages/notFound/notFound.page';
+
+const CharacterPage = lazy(() => import('../pages/character/character.page'));
+const CharactersPage = lazy(() => import('../pages/characters/characters.page'));
+
+function Routes() {
+  return useRoutes([
+    {
+      path: '/characters',
+      element: <CharactersPage />,
+    },
+    { path: 'character/:id', element: <CharacterPage /> },
+    { path: '404', element: <NotFoundPage /> },
+    { path: '*', element: <Navigate to="/404" /> },
+    { path: '', element: <Navigate to="/characters" /> }, // Redirect from the main page to the 'characters' page
+  ]);
+}
+
+export default Routes;
