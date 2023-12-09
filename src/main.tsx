3e3f4035b1754ed35c53ad5caed8456cb1b96f33
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-09 15:33:03 +0100

    created notifications API, wrapped the app inside a context

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { NotificationProvider } from './context/notification.context';
 import Routes from './config/routes';
 import Loader from './lib/loader/loader.cmp';
 import './index.css';
@@ -15,9 +16,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <Suspense fallback={<Loader />}>
-            <Routes />
-          </Suspense>
+          <NotificationProvider>
+            <Suspense fallback={<Loader />}>
+              <Routes />
+            </Suspense>
+          </NotificationProvider>
         </BrowserRouter>
       </QueryClientProvider>
     </LocalizationProvider>
