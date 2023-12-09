af42c9c236f68642f9e2f647d00f0923177401bb
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-09 15:29:42 +0100

    wrapped the app with 'LocalizationProvider', 'QueryClientProvider', 'BrowserRouter' and Suspense with the loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Routes from './config/routes';
+import Loader from './lib/loader/loader.cmp';
 import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Suspense fallback={<Loader />}>
+            <Routes />
+          </Suspense>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </LocalizationProvider>
   </React.StrictMode>,
 );
