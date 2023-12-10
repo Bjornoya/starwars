2a68ca99dd5556e02ef99aa70f1668451ee25115
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-10 15:08:34 +0100

    updated the api func, added the search

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,8 +2,8 @@ import { API } from './settings';
 import { ICharacters } from './interface';
 import { mapCharacters } from './utils';
 
-export const getCharacters = async (page: number) => {
-  const response = await fetch(`${API}/people/?page=${page}`);
+export const getCharacters = async (query: string) => {
+  const response = await fetch(`${API}/people/${query}`);
   const result: ICharacters = await response.json();
 
   return mapCharacters(result);
--- src/pages/characters/characters.page.tsx
+++ src/pages/characters/characters.page.tsx
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { Box, TextField } from '@mui/material';
 import Layout from '../../lib/layout/layout.cmp';
 import CharactersTable from './components/charactersTable/charactersTable.cmp';
 import { columns, initialTableState } from './characters.utils';
 import { getCharacters } from '../../config/api';
 
 function CharactersPage() {
+  const [search, setSearch] = useState('');
   const { page = 1 } = useParams();
   const navigate = useNavigate();
-  const currentPage = Number(page);
   const pageIndex = Number(page) - 1;
+  const query = search ? `?search=${search}&page=${page}` : `?page=${page}`; // Push it to the url in the future
 
   const {
     data = initialTableState,
     isInitialLoading,
     isFetching,
   } = useQuery({
-    queryKey: ['characters', [currentPage]],
-    queryFn: () => getCharacters(currentPage),
+    queryKey: ['characters', [query]],
+    queryFn: () => getCharacters(query),
     placeholderData: keepPreviousData,
   });
 
-  const onPageChange = (newPage: number) =>
-    newPage === 1 ? navigate('/characters/') : navigate(`/characters/${newPage}`);
+  const onPageChange = (newPage: number) => {
+    const url = newPage === 1 ? '/characters/' : `/characters/${newPage}`;
+    navigate(url);
+  };
+
+  const onSearch = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+    navigate('/characters/');
+  };
 
   return (
     <Layout>
+      <Box sx={{ margin: '24px 0' }}>
+        <TextField
+          onChange={onSearch}
+          sx={{ width: '40%' }}
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          size="small"
+        />
+      </Box>
       <CharactersTable
         columns={columns}
         rows={data.results}
