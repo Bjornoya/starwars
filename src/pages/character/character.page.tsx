1cd666af13fcff216d76b8807b47424e4544deda
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-10 17:18:37 +0100

    created bare-bones character page with initial state

--- src/pages/character/character.page.tsx
+++ src/pages/character/character.page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 import Layout from '../../lib/layout/layout.cmp';
+import { initialCharacterState } from './character.utils';
+import { getCharacter } from '../../config/api';
+import Planet from './components/planet.cmp';
 
 function CharacterPage() {
+  const { id } = useParams();
+  const { data = initialCharacterState } = useQuery({
+    queryKey: ['character', [id]],
+    queryFn: () => getCharacter(id),
+  });
+
+  console.log(data);
   return (
     <Layout>
       <div>Character PAGE</div>
+      <Planet url={data.homeworld} />
     </Layout>
   );
 }
---
+++ src/pages/character/character.utils.ts
@@ -0,0 +1,18 @@
+export const initialCharacterState = {
+  name: '',
+  height: '',
+  mass: '',
+  hair_color: '',
+  skin_color: '',
+  eye_color: '',
+  birth_year: '',
+  gender: '',
+  homeworld: '',
+  films: [],
+  species: [],
+  vehicles: [],
+  starships: [],
+  created: '',
+  edited: '',
+  url: '',
+};
