849cad63a14d1ed06354d41aaf93c23234d448a4
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-10 17:16:44 +0100

    created the 'getByUrl', 'getCharacter' and 'getPlanet' fns

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,28 @@
 import { API } from './settings';
-import { ICharacters } from './interface';
-import { mapCharacters } from './utils';
+import { ICharacter, ICharacters } from './interface';
+import { mapCharacters, mapPlanet } from './utils';
+
+export const getByUrl = async (url: string) => {
+  const response = await fetch(url);
+  return response.json();
+};
 
 export const getCharacters = async (query: string) => {
   const response = await fetch(`${API}/people/${query}`);
   const result: ICharacters = await response.json();
 
   return mapCharacters(result);
+};
+
+export const getCharacter = async (id: string) => {
+  const response = await fetch(`${API}/people/${id}`);
+  const result: ICharacter = await response.json();
+
+  return result;
+};
+
+export const getPlanet = async (url: string) => {
+  const result = await getByUrl(url);
+
+  return mapPlanet(result);
 };
