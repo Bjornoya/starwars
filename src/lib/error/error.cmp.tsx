941879a08881a31d9412ea13ba57ee0f73e346e6
Gleb Ambrazhevich <glebikx@gmail.com>
2023-12-11 01:45:46 +0100

    added the error boundary

---
+++ src/lib/error/error.cmp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function Error() {
+  return (
+    <Box sx={{ width: '100%', height: '100vh' }}>
+      <div>Something went wrong.</div>
+    </Box>
+  );
+}
+
+export default Error;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { NotificationProvider } from './context/notification.context';
 import Routes from './config/routes';
 import Loader from './lib/loader/loader.cmp';
+import Error from './lib/error/error.cmp';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -17,9 +19,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <NotificationProvider>
-            <Suspense fallback={<Loader />}>
-              <Routes />
-            </Suspense>
+            <ErrorBoundary fallback={<Error />}>
+              <Suspense fallback={<Loader />}>
+                <Routes />
+              </Suspense>
+            </ErrorBoundary>
           </NotificationProvider>
         </BrowserRouter>
       </QueryClientProvider>
